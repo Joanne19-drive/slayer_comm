e81becfaf5a25e26e1e2712a2fcba892cdce891c
고객데이터플랫폼팀 곽민지 <kwak.mj@sk.com>
2023-06-23 12:02:11 +0900

    v2

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from typing import Dict
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: Dict[str, WebSocket] = {}
+
+    async def connect(self, websocket: WebSocket, username: str):
+        await websocket.accept()
+        self.active_connections[username] = websocket
+
+    def disconnect(self, username: str):
+        del self.active_connections[username]
+
+    async def broadcast(self, message: str, sender: str):
+        for user, connection in self.active_connections.items():
+            await connection.send_text(f"[{sender}] {message}")
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -32,7 +49,39 @@ async def quiz_result(request: Request):
     for i in range(8):
         if answer[i] == form_data.get(f"question{i + 1}"):
             score += 1
-    return templates.TemplateResponse("quiz_result.html", {"request": request, "score": score, "category": get_score_category(score)})
+    return templates.TemplateResponse("quiz_result.html",
+                                      {"request": request, "score": score, "category": get_score_category(score)})
+
+
+@app.get("/archive", response_class=HTMLResponse)
+async def character_archive(request: Request):
+    return templates.TemplateResponse("archive.html", {"request": request})
+
+
+@app.get("/chatroom", response_class=HTMLResponse)
+async def chatroom(request: Request):
+    return templates.TemplateResponse("chat.html", {"request": request})
+
+
+manager = ConnectionManager()
+
+
+@app.websocket("/ws/{username}")
+async def websocket_endpoint(websocket: WebSocket, username: str):
+    await manager.connect(websocket, username)
+    await manager.broadcast(f"{username}가 채팅방에 참여했습니다.", "* ")
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.broadcast(data, username)
+    except WebSocketDisconnect:
+        manager.disconnect(username)
+        await manager.broadcast(f"{username}가 채팅방을 떠났습니다.", "* ")
+
+
+@app.get("/participants")
+async def show_participants():
+    return list(manager.active_connections.keys());
 
 
 def get_score_category(score):
